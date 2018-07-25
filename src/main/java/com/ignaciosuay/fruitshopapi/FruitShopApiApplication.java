084b2851aad9e0a158ad7a504b73a143ea5d1182
44087895 <ignacio.s.mas@noexternalmail.hsbc.com>
2018-07-25 18:28:38 +0100

    Create base Spring project

---
+++ src/main/java/com/ignaciosuay/fruitshopapi/FruitShopApiApplication.java
@@ -0,0 +1,12 @@
+package com.ignaciosuay.fruitshopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitShopApiApplication.class, args);
+	}
+}
